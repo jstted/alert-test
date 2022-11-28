10d9b9a4a7acf32c159b1ae3c10bcd297678ecb6
jstted <tedeev17@gmail.com>
2022-11-28 12:34:32 +0300

    Initial Commit

---
+++ alert test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alert test
+//
+//  Created by Mikhail Tedeev on 28.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
